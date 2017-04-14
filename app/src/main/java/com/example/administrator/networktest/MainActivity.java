1f39e219d72713e881d3fcd179d51b389b8ebc7b
goobee <goobee_yuer0716@163.com>
2017-04-14 17:13:18 +0800

    The new

--- app/src/main/java/com/example/administrator/networktest/MainActivity.java
+++ app/src/main/java/com/example/administrator/networktest/MainActivity.java
@@ -1,6 +1,5 @@
 package com.example.administrator.networktest;
 
-import android.app.DownloadManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
@@ -8,12 +7,8 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 
-import java.io.BufferedReader;
 import java.io.IOException;
-import java.io.InputStream;
-import java.io.InputStreamReader;
-import java.net.HttpURLConnection;
-import java.net.URL;
+
 
 import okhttp3.OkHttpClient;
 import okhttp3.Request;
